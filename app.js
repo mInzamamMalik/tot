5b92ad3be3c84b9ba5ef2827f6fed493bb9001e8
malikasinger1@gmail.com <malikasinger1@gmail.com>
2016-04-13 19:59:20 +0500

    working more fine but still suffering from bugs

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ function appController($scope){
 
     $scope.project = {
 
-        Todo : [],
-        Doing : [],
+        Todo :["1.0", "2.5", "3.0", "4.1"],
+        Doing : ["6.0", "7.5", "8.4", "9.1"],
         Review : [],
         Done : []
 
