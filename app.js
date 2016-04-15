3dfd5991917938ebd0d7b54df4f4494562e84679
malikasinger1@gmail.com <malikasinger1@gmail.com>
2016-04-15 22:00:34 +0500

    addign and removing is working

--- app.js
+++ app.js
@@ -6,19 +6,26 @@ angular.module("myApp", ["ngMaterial","angular-sortable-view","ngMdIcons"])
 function appController($scope,$mdDialog){
 
     $scope.project = {
-        Todo :[],
-        Doing : [],
-        Review : [],
-        Done : []
+        Todo :{},
+        Doing : {},
+        Review : {},
+        Done : {}
     };
 
     var ref = new Firebase("https://teamofteam.firebaseio.com/projects");
 
-    ref.child("projectId1234").child("Todo").on("child_added",function(val){
-
-        $scope.project.Todo.push(val.val());
-
+    ref.child("projectId1234").child("Todo").on("child_added",function(snap){
+        console.log(snap.key(), snap.val());
+        $scope.project.Todo[snap.key()] = snap.val();
+        $scope.$digest();
     });
+
+    ref.child("projectId1234").child("Todo").on("child_removed",function(val){
+        console.log(val.val());
+        delete $scope.project.Todo[val.key()];
+        $scope.$digest();
+    });
+
     ref.child("projectId1234").child("Doing").on("child_added",function(val){
 
 
