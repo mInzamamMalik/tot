a89b489dc83f445d0ff29037807fef77da6b2d48
malikasinger1@gmail.com <malikasinger1@gmail.com>
2016-04-15 21:25:45 +0500

    simple push done

--- app.js
+++ app.js
@@ -5,13 +5,39 @@ angular.module("myApp", ["ngMaterial","angular-sortable-view","ngMdIcons"])
 
 function appController($scope,$mdDialog){
 
-
     $scope.project = {
-
-        Todo :["chiken tikka","tandoori chargha","malai boti"],
+        Todo :[],
         Doing : [],
-        Review : [  "nihaari","chicken karahi" , "ui design"],
-        Done : [ ]
+        Review : [],
+        Done : []
     };
+
+    var ref = new Firebase("https://teamofteam.firebaseio.com/projects");
+
+    ref.child("projectId1234").child("Todo").on("child_added",function(val){
+
+        $scope.project.Todo.push(val.val());
+
+    });
+    ref.child("projectId1234").child("Doing").on("child_added",function(val){
+
+
+    });
+    ref.child("projectId1234").child("Review").on("child_added",function(val){
+
+
+    });
+    ref.child("projectId1234").child("Done").on("child_added",function(val){
+
+
+    });
+
+
+
+     $scope.addNewTask = function(newTask){
+
+        ref.child("projectId1234").child("Todo").push(newTask);
+        $scope.newTask = "";
+    }
 
 }
