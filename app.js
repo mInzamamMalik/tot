ea9a708436dfb649b6027c6dcff41c9519e47417
malikasinger1@gmail.com <malikasinger1@gmail.com>
2016-04-19 18:25:41 +0500

    thora kaam hua hai thora nhe hua :-)

--- app.js
+++ app.js
@@ -1,50 +1,94 @@
-angular.module("myApp", ["ngMaterial","angular-sortable-view","ngMdIcons"])
+angular.module("myApp", ["ngMaterial","angular-sortable-view"])
 
-.controller("appController",[ "$scope", "$mdDialog",  appController ]);
+.controller("appController",[ "$scope",  app ]);
 
 
-function appController($scope,$mdDialog){
+function app($scope){
 
     $scope.project = {
-        Todo :{},
-        Doing : {},
-        Review : {},
-        Done : {}
+        Todo :[],
+        Doing : [],
+        Review : [],
+        Done : []
     };
 
     var ref = new Firebase("https://teamofteam.firebaseio.com/projects");
 
+    //when a data is added
     ref.child("projectId1234").child("Todo").on("child_added",function(snap){
-        console.log(snap.key(), snap.val());
-        $scope.project.Todo[snap.key()] = snap.val();
+        //console.log(snap.key(), snap.val());
+        var temp = {};
+        temp.key = snap.key();
+        temp.value = snap.val();
+
+        $scope.project.Todo.push(temp);
+        $scope.$apply();
+    });
+
+    //when a data is removed
+    ref.child("projectId1234").child("Todo").on("child_removed",function(snap){
+
+        for(i=0; i<$scope.project.Todo.length; i++){
+            if($scope.project.Todo[i].key == snap.key()){
+                $scope.project.Todo.splice(i,1);
+                console.log("delete executed");
+                break;
+            }
+        }
         $scope.$digest();
     });
 
-    ref.child("projectId1234").child("Todo").on("child_removed",function(val){
-        console.log(val.val());
-        delete $scope.project.Todo[val.key()];
-        $scope.$digest();
-    });
+    //when removed an item form local list any source or library
+    $scope.$watch("project.Todo", function (newValue, oldValue) {
 
-    ref.child("projectId1234").child("Doing").on("child_added",function(val){
+        if(!oldValue.length){ // it means array is already empty
+            //alert(oldValue.length);
+            //return;
+        }else if(!newValue.length){ // it means array mai aik he element tha or ab array empty ho chuka ahi
+
+            $scope.delete(oldValue[0].key); // also delete from firebase
+
+        }else if(newValue.length < oldValue.length){ // an index is deleted from this list
+            for(i=0; i< oldValue.length; i++){
+
+                if(newValue[i].key !=  oldValue[i].key){
+                    console.log( oldValue[i]," is deleted");
+                    $scope.delete(oldValue[i].key); // also delete from firebase
+                    break;
+                }
+            }
+        }
 
 
-    });
-    ref.child("projectId1234").child("Review").on("child_added",function(val){
+
+        }, true // Object equality (not just reference).
+    );
+
+    //
+    //ref.child("projectId1234").child("Doing").on("child_added",function(val){
+    //
+    //
+    //});
+    //ref.child("projectId1234").child("Review").on("child_added",function(val){
+    //
+    //
+    //});
+    //ref.child("projectId1234").child("Done").on("child_added",function(val){
+    //
+    //
+    //});
 
 
-    });
-    ref.child("projectId1234").child("Done").on("child_added",function(val){
-
-
-    });
-
-
-
+     //add a task to firebase "not to local array"
      $scope.addNewTask = function(newTask){
 
         ref.child("projectId1234").child("Todo").push(newTask);
         $scope.newTask = "";
-    }
+     };
+
+     //remove a task from firebase "not from local array"
+     $scope.delete = function(key){
+        ref.child("projectId1234").child("Todo").child(key).set(null);
+     };
 
 }
